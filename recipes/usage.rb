e9accdf7e092161165958d76626524de64bc593a
Patrick Wright <patrick@getchef.com>
2014-05-30 12:32:29 -0700

    chef_user

--- recipes/usage.rb
+++ recipes/usage.rb
@@ -1,9 +1,9 @@
-user "fred" do
+chef_user "fred" do
   action :create
   first_name 'Fred'
   #middle_name '' optional
   last_name 'Mercury'
   email 'fred@getchef.com'
   password 'bohemian'
-  pem_file '/pem/filename.pem' # optional
+  pem_file '/path/filename.pem' # optional
 end
