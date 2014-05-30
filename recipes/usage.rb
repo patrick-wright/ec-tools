ff73f5debc05bde53a31d6f651e4fdd7ae5f99b9
Patrick Wright <patrick@getchef.com>
2014-05-30 12:34:14 -0700

    add comments

--- recipes/usage.rb
+++ recipes/usage.rb
@@ -1,9 +1,9 @@
-chef_user "fred" do
-  action :create
-  first_name 'Fred'
-  #middle_name '' optional
-  last_name 'Mercury'
-  email 'fred@getchef.com'
-  password 'bohemian'
+chef_user "fred" do # name => username
+  action :create # default
+  first_name 'Fred' # required
+  # middle_name '' optional
+  last_name 'Mercury' # required
+  email 'fred@getchef.com' # required
+  password 'bohemian' # required
   pem_file '/path/filename.pem' # optional
 end
