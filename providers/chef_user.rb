e75022f237e0d454f735ffb2cfae96539ec3c96a
Patrick Wright <patrick@getchef.com>
2014-06-03 09:33:52 -0700

    subclassing LWRPs

--- providers/chef_user.rb
+++ providers/chef_user.rb
@@ -1,40 +1,47 @@
 require 'chef/config'
+require 'chef/provider/lwrp_base'
 
-def whyrun_supported?
-  true
-end
+class Chef::Provider::ChefUser < Chef::Provider::LWRPBase
+  def whyrun_supported?
+    true
+  end
 
-def create_chef_user
+  def create_chef_user
 
-end
+  end
 
-def user_exists?
-  chef_rest = Chef::REST.new(configure_chef)
-  result = chef_rest.get_rest("users/#{@new_resource.username}") # what happens when user does not exist (exc or nil or '')
-end
+  def delete_chef_user
 
-action :create do
-  unless user_exists? 
-    converge_by("Create #{ @new_resource }") do
-      create_chef_user
+  end
+
+  def user_exists?
+    chef_rest = Chef::REST.new(configure_chef)
+    result = chef_rest.get_rest("users/#{@new_resource.username}") # what happens when user does not exist (exc or nil or '')
+  end
+
+  action :create do
+    unless user_exists? 
+      converge_by("Create #{ @new_resource }") do
+        create_chef_user
+      end
     end
   end
-end
 
 
-action :delete do
-  if user_exists?
-    converge_by("Delete #{ @new_resource }") do
-      delete_chef_user
+  action :delete do
+    if user_exists?
+      converge_by("Delete #{ @new_resource }") do
+        delete_chef_user
+      end
     end
   end
-end
 
-# action :modify do
-# end
+  # action :modify do
+  # end
 
-def configure_chef
-  Chef::Config[:node_name] = node['chef']['config']['node_name']
-  Chef::Config[:client_key] = node['chef']['config']['client_key']
-  Chef::Config[:chef_server_url] = node['chef']['config']['chef_server_root']
+  def configure_chef
+    Chef::Config[:node_name] = node['chef']['config']['node_name']
+    Chef::Config[:client_key] = node['chef']['config']['client_key']
+    Chef::Config[:chef_server_url] = node['chef']['config']['chef_server_root']
+  end
 end
--- resources/chef_user.rb
+++ resources/chef_user.rb
@@ -1,12 +1,18 @@
-actions :create, :delete, :modify
-default_action :create
+require 'chef/resource/lwrp_base'
 
-attribute :username, :name_attribute => true, :required => true, :kind_of => String 
-attribute :first_name, :required => true, :kind_of => String
-attribute :middle_name, :kind_of => String
-attribute :last_name, :required => true, :kind_of => String
-attribute :email, :required => true, :kind_of => String # email type?
-attribute :password, :required => true, :kind_of => String
-attribute :pem_file, :kind_of => String
+class Chef::Resource::ChefUser < Chef::Resource::LWRPBase
+  self.resource_name = 'chef_user'
 
-attr_accessor :exists # check if user exists
+  actions :create, :delete, :modify
+  default_action :create
+
+  attribute :username, :name_attribute => true, :required => true, :kind_of => String 
+  attribute :first_name, :required => true, :kind_of => String
+  attribute :middle_name, :kind_of => String
+  attribute :last_name, :required => true, :kind_of => String
+  attribute :email, :required => true, :kind_of => String # email type?
+  attribute :password, :required => true, :kind_of => String
+  attribute :pem_file, :kind_of => String
+
+  attr_accessor :exists # check if user exists
+end
