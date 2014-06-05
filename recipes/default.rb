fa6cb027397dbb2fa07acb5875819bc0ccc5f03a
Patrick Wright <patrick@getchef.com>
2014-06-05 14:24:24 -0700

    added knife opc gem with goodies

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,38 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# create the client key file if private key content is provided
+if node['chef']['config']['private_key_raw']
+  directory File.dirname(node['chef']['config']['client_key'])
+
+  file node['chef']['config']['client_key'] do
+    content node['chef']['config']['private_key_raw']
+  end
+end
+
 chef_gem 'knife-opc'
+
+directory File.dirname(node['knife']['admin']['config']) do
+  recursive true
+end
+
+template node['knife']['admin']['config'] do
+  source 'knife-pivotal.rb.erb'
+  variables(:node_name => node['chef']['config']['node_name'],
+             :client_key => node['chef']['config']['client_key'],
+             :chef_server_root => node['chef']['config']['chef_server_root'])
+  mode '644'
+end
+
+directory File.dirname(node['knife']['admin']['bin']) do
+  recursive true
+end
+
+template node['knife']['admin']['bin'] do
+  source 'knife-opc.erb'
+  variables(:knife_config => node['knife']['admin']['config'])
+  mode '777'
+end
 
 file '/etc/profile.d/chef.sh' do
   content 'export PATH=${PATH}:/opt/opscode/embedded/bin'
